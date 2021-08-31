f9ace30f74acfabe76638bf88503cc8ef2ad0531
zhao00627 <18777557501@163.com>
2021-08-31 22:49:31 +0800

    second commit

--- src/main/java/com/atguigu/git/GitDemo02.java
+++ src/main/java/com/atguigu/git/GitDemo02.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class GitDemo02 {
     public static void main(String[] args) {
         System.out.println("github hello!");
+        System.out.println("github hello!");
+        System.out.println("github hello!");
     }
 }
